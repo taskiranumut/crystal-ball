f3087bec7bb22a6c1d1f1eab5f6fe99f0450970b
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-05-10 23:44:17 +0300

    feat: add fake network latency for asynchronous behavior

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+async function fakeNetworkLatency() {
+  return new Promise((res) => {
+    setTimeout(res, Math.random() * 800);
+  });
+}
+
 window.addEventListener("load", () => {
   const appRoot = document.querySelector("#app-root");
   console.log("appRoot Loaded: ", appRoot);
