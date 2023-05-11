fb625bdb91aa8c375a742d0895cb85f90c885ce5
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-05-11 21:32:36 +0300

    feat: add general element selector

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-async function fakeNetworkLatency() {
+const fakeNetworkLatency = async () => {
   return new Promise((res) => {
     setTimeout(res, Math.random() * 800);
   });
+};
+
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (!element) {
+    console.error(`Element not found: ${selector}`);
+    return null;
 }
+  return element;
+};
+
 
 window.addEventListener("load", () => {
 
