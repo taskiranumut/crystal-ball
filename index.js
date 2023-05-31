2c031064a7b8e3f56c339cb5b41ffe7a839a4187
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-05-31 22:29:41 +0300

    refactor: set TAGS_DATA as global variable

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+/**
+ * An array of objects, each representing a tag to be used in the application.
+ * Each tag object includes:
+ * @property {string} value - The internal value of the tag.
+ * @property {string} display - The display name of the tag.
+ * @property {boolean} isActive - A flag indicating if the tag is active.
+ */
+const TAGS_DATA = [
+  { value: "all", display: "All", isActive: true },
+  { value: "politics", display: "Politics", isActive: false },
+  { value: "magazine", display: "Magazine", isActive: false },
+  { value: "finance", display: "Finance", isActive: false },
+  { value: "technology", display: "Technology", isActive: false },
+  { value: "science", display: "Science", isActive: false },
+  { value: "humanity", display: "Humanity", isActive: false },
+  { value: "society", display: "Society", isActive: false },
+];
+
 let globalCountdownInterval;
 const eventTracker = {};
 const choicesInstances = {};
@@ -1360,17 +1378,6 @@ const toggleButtonsDisabledStatus = (
  */
 const fillTagButtonList = (tagButtonListEl) => {
   validateIsHtmlElement(tagButtonListEl);
-
-  const TAGS_DATA = [
-    { value: "all", display: "All", isActive: true },
-    { value: "politics", display: "Politics", isActive: false },
-    { value: "magazine", display: "Magazine", isActive: false },
-    { value: "finance", display: "Finance", isActive: false },
-    { value: "technology", display: "Technology", isActive: false },
-    { value: "science", display: "Science", isActive: false },
-    { value: "humanity", display: "Humanity", isActive: false },
-    { value: "society", display: "Society", isActive: false },
-  ];
 
   const tagButtons = TAGS_DATA.map((data) => getTagButtonTemplate(data)).join(
     ""
