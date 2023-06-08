3965c2af602161f8293f090ff115b758ca736582
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-06-08 22:51:39 +0300

    chore: change animate.css cdn with npm package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "flatpickr";
 import "flatpickr/dist/themes/dark.css";
 import "choices.js/public/assets/styles/choices.css";
 import "./assets/css/styles.css";
+import "animate.css";
 import initApp from "./app";
 
 window.addEventListener("load", initApp);
