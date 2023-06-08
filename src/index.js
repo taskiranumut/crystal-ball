bd8c84e427f59fa1bd3b8ed7b6b9687462edf52e
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-06-08 22:40:09 +0300

    chore: change flatpickr cdn with npm package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import initApp from "./app";
+import "flatpickr";
+import "flatpickr/dist/themes/dark.css";
 import "./assets/css/styles.css";
+import initApp from "./app";
 
 window.addEventListener("load", initApp);
